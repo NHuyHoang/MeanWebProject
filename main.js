ca03567e653d54796fca5003f98dbda9872bbed1
NHuyHoang <14110066@student.hcmute.edu.vn>
2017-10-25 15:56:04 +0700

    adding UI

--- main.js
+++ main.js
@@ -11,37 +11,19 @@ const fetch = require('./routes/fetch');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(uri, { useMongoClient: true })
-.then(() => {
-	console.log('Connected to db at ', uri);
-	return mongoose.connection;
-})
-.catch(err => {
-	console.log('Fail to connect ' + err);
-});
+	.then(() => {
+		console.log('Connected to db at ', uri);
+		return mongoose.connection;
+	})
+	.catch(err => {
+		console.log('Fail to connect ' + err);
+	});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use('/fetchdata',fetchdata);
-app.use('/fetch',fetch);
+app.use('/fetchdata', fetchdata);
+app.use('/fetch', fetch);
 
 app.listen(3000, () => {
 	console.log('listening on port 3000');
 });
-
-let MobileSchema = require ('./models/products/electronics/mobile');
-let mobile = new MobileSchema({
-	description:  "des",
-	productname: "productname",
-	state: "new",
-	producer: "producer",
-	cost: 5000,
-	currency: "USD",
-	categoryid:"elt",
-	guarantee:"3years",
-	paymentmethod:"VISA",
-	memory: "16gb",
-	ram: "2gb",
-	megapixel: "12mgpx",
-	abcd:"123" // document khong hop le
-});
-console.log(mobile);
